0ed96182d543a5ea0ea021a61088a91bad2ecb3b
Ketan Ahuja <56186989+KETANAHUJA@users.noreply.github.com>
2021-05-28 11:25:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -91,7 +91,7 @@ button.onclick = function () {
         setColor(temp)
         changeBoard(board)
     }
-    xhrRequest.open('get', 'https://sugoku.herokuapp.com/board?difficulty=easy')
+    xhrRequest.open('get', 'https://sugoku.herokuapp.com/board?difficulty=hard')
     //we can change the difficulty of the puzzle the allowed values of difficulty are easy, medium, hard and random
     xhrRequest.send()
 }
